f18632178fba10e92ade708cc0c58e94dd45b6d8
3bouda <abdelaliiheb@gmail.com>
2024-07-02 04:26:37 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tp4.Startup))]
+namespace tp4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
